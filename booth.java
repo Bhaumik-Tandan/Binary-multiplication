d086be318f65263fb53d81c1eac729cdd1f5621e
Bhaumik-Tandan <62440699+Bhaumik-Tandan@users.noreply.github.com>
2021-03-24 19:03:16 +0530

    Update booth.java

--- booth.java
+++ booth.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class booth {
-    
+public class binary {
+    static int n;
     static String pad(String qr,int br)
     {
         String s="";
@@ -12,10 +12,11 @@ public class booth {
 
     static String add(String p,String d)
     {
-        int l=p.length()>p.length()?p.length():d.length();
-        p=pad(p,l);
-        d=pad(d,l);
+        int l;
+        p=pad(p,n);
+        d=pad(d,n);
         int c=0;
+        l=n;
         l-=1;
         int a;
         while(l>=0){
@@ -29,11 +30,11 @@ public class booth {
     static void mul(String qr,String br)
     {
         System.out.printf("%20s %10s %10s %10s %5s\n","Comment","AC","QR","Q(n+1)","SC");
-        int sc=qr.length();
-        String ac=pad("0",sc>br.length()?sc:br.length());
+        int sc=8;
+        String ac=pad("0",n);
         char q1='0';
         int p=sc;
-        System.out.printf("%20s %10s %10s %10s %5s\n","",ac,qr,q1,sc);
+        System.out.printf("%20s %10s %10s %10s %5s\n","",ac,pad(qr,n),q1,sc);
         while(sc>0)
         {
             //condition for 10 we will subtract
@@ -50,7 +51,7 @@ public class booth {
             }
             else if(q1=='1' && qr.charAt(p-1)=='0')
             {
-                System.out.printf("%20s %10s\n","AC+BR",br);
+                System.out.printf("%20s %10s\n","AC+BR",pad(br,n));
                String s="";
                ac=add(ac,br);
                System.out.printf("%20s %10s\n","",ac);
@@ -58,7 +59,7 @@ public class booth {
             q1=qr.charAt(p-1);
             qr=ac.charAt(ac.length()-1)+qr.substring(0,p-1);
             ac=ac.charAt(0)+ac.substring(0,ac.length()-1);
-            System.out.printf("%20s %10s %10s %10s %5s\n","SHIFT",ac,qr,q1,--sc);
+            System.out.printf("%20s %10s %10s %10s %5s\n","SHIFT",ac,pad(qr,n),q1,--sc);
         }
 
     }
@@ -69,7 +70,8 @@ public class booth {
         String mp=s.next();
         System.out.print("\nEnter the Multiplicand: ");
         String md=s.next();*/
-        String qr="10011",br="10111";
-        mul(qr,br);
+        String qr="10111",br="11011";
+        n=8;
+        mul(pad(qr,n),pad(br,n));
     }
 }

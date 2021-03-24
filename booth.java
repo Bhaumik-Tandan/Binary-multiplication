0faf6278fc273ae523b280ff386e5a518ce98f25
Bhaumik-Tandan <62440699+Bhaumik-Tandan@users.noreply.github.com>
2021-03-24 20:57:32 +0530

    Update booth.java

--- booth.java
+++ booth.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class binary {
+public class a {
     static int n;
     static String pad(String qr,int br)
     {
@@ -12,12 +12,8 @@ public class binary {
 
     static String add(String p,String d)
     {
-        int l;
-        p=pad(p,n);
-        d=pad(d,n);
+        int l=n-1;
         int c=0;
-        l=n;
-        l-=1;
         int a;
         while(l>=0){
             a=(Character.getNumericValue((d.charAt(l)))^(Character.getNumericValue((p.charAt(l)))))^c;
@@ -30,48 +26,46 @@ public class binary {
     static void mul(String qr,String br)
     {
         System.out.printf("%20s %10s %10s %10s %5s\n","Comment","AC","QR","Q(n+1)","SC");
-        int sc=8;
+        int sc=n;
         String ac=pad("0",n);
         char q1='0';
-        int p=sc;
-        System.out.printf("%20s %10s %10s %10s %5s\n","",ac,pad(qr,n),q1,sc);
+        System.out.printf("%20s %10s %10s %10s %5s\n","",ac,qr,q1,sc);
         while(sc>0)
         {
             //condition for 10 we will subtract
-            if(q1=='0' && qr.charAt(p-1)=='1')
+            if(q1=='0' && qr.charAt(n-1)=='1')
             {
-                
                String s="";
-               for(int i=0;i<br.length();i++)
+               for(int i=0;i<n;i++)
                s=s+(br.charAt(i)=='0'?"1":"0");//finding complement
-               s=add("1",s);//2's complement
+               s=add(pad("1",n),s);//2's complement
                System.out.printf("%20s %10s\n","AC+BR'+1",s);
                ac=add(ac,s);
                System.out.printf("%20s %10s\n","",ac);
             }
-            else if(q1=='1' && qr.charAt(p-1)=='0')
+            else if(q1=='1' && qr.charAt(n-1)=='0')
             {
-                System.out.printf("%20s %10s\n","AC+BR",pad(br,n));
+                System.out.printf("%20s %10s\n","AC+BR",br);
                String s="";
                ac=add(ac,br);
                System.out.printf("%20s %10s\n","",ac);
             }
-            q1=qr.charAt(p-1);
-            qr=ac.charAt(ac.length()-1)+qr.substring(0,p-1);
-            ac=ac.charAt(0)+ac.substring(0,ac.length()-1);
-            System.out.printf("%20s %10s %10s %10s %5s\n","SHIFT",ac,pad(qr,n),q1,--sc);
+            q1=qr.charAt(n-1);
+            qr=ac.charAt(n-1)+qr.substring(0,n-1);
+            ac=ac.charAt(0)+ac.substring(0,n-1);
+            System.out.printf("%20s %10s %10s %10s %5s\n","SHIFT",ac,qr,q1,--sc);
         }
 
     }
 
     public static void main(String[] args)  {
-      /*  Scanner s=new Scanner(System.in);
+        Scanner s=new Scanner(System.in);
         System.out.print("Enter the Multiplier: ");
-        String mp=s.next();
+        String qr=s.next();
         System.out.print("\nEnter the Multiplicand: ");
-        String md=s.next();*/
-        String qr="10111",br="11011";
-        n=8;
+        String br=s.next();
+        System.out.print("\nEnter the Number of bits: ");
+        n=s.nextInt();
         mul(pad(qr,n),pad(br,n));
     }
 }

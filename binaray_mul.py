c001b90ecd415f8be6ed3808ea7b6b52aac6b622
Bhaumik-Tandan <62440699+Bhaumik-Tandan@users.noreply.github.com>
2021-05-21 12:00:17 +0530

    Update binaray_mul.py

--- binaray_mul.py
+++ binaray_mul.py
@@ -17,18 +17,46 @@ def fun(a,b):
 o=input("19BIT0292\n1.Binary\n2.Decimal\nEnter your choice: ")
 mul=input("ENTER THE MULTIPLICAND: ")
 mp=input("ENTER THE MULTIPLIER: ")
+k=int(input("\nEnter the number of bits: "))
+f=0
+bi="0"*k
 if o=="2":
-    mul=str(bin(int(mul)))[2:]
-    mp=str(bin(int(mp)))[2:]
+    mul=int(mul)
+    if mul<0:
+        mul*=-1
+        _,mul=fun(str(bin(mul)[2:]),bi)
+        mul=list(mul)
+        for i in range(len(mul)):
+            if mul[i]=="0":
+                mul[i]="1"
+            else:
+                mul[i]="0"
+        _,mul=fun(mul,"1")
+        f+=1
+    else:
+        _,mul=fun(str(bin(mul))[2:],bi)             
+    mp=int(mp)
+    if mp<0:
+        mp*=-1
+        _,mp=fun(str(bin(mp)[2:]),bi)
+        mp=list(mp)
+        for i in range(len(mp)):
+            if mp[i]=="0":
+                mp[i]="1"
+            else:
+                mp[i]="0"
+        _,mp=fun(mp,"1")       
+        f+=1
+    else:
+         _,mp=fun(str(bin(mp))[2:],bi)           
 e=0
 q=mp
-tu="0"
+a="0"
 for i in range(1,len(q)):
-    tu+="0"
+    a+="0"
 i=len(q)
 sc=str(bin(i))[2:]
-t=[["","0",tu,q,sc]]
-a=tu
+t=[["","0",a,q,sc]]
 p=i
 i-=1
 while i>=0:
@@ -45,5 +73,9 @@ while i>=0:
     e=0
     t.append(["SHIFT",e,a,q,sc.zfill(len(str(bin(p))[2:]))])
 print(tabulate(t,["OPERATION","E","A","Q","SC"]))
-print("\nRESULT in BINARY= "+a+q)
-print("\nRESULT in DECIMAL= "+str(int(a+q,2)))
+if f==0:
+    print("\nRESULT in BINARY= "+a+q)
+    print("\nRESULT in DECIMAL= "+str(int(a+q,2)))
+elif f==2:
+    print("\nRESULT in BINARY= "+(a+q)[k:])
+    print("\nRESULT in DECIMAL= "+str(int((a+q)[k:],2)))
